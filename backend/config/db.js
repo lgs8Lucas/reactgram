f572aa9347d526aebe140aa472a85999bdce5eaa
Lucas <lgs.08lucas@gmail.com>
2024-12-04 15:44:02 -0300

    Criado a conexão com o banco

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,18 @@
 const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const conn = async () => {
+	try {
+		const dbConn = await mongoose.connect(
+			`mongodb+srv://${dbUser}:${dbPass}@cluster0.o898o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+		);
+		console.log("Conectou ao banco");
+		return dbConn;
+	} catch (er) {
+		console.log(er);
+	}
+};
+
+conn();
+
+module.exports = conn;
