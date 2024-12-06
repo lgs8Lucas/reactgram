8ccd6dfa9ac04f73473fd129c916d663906ea8fd
Lucas <lgs.08lucas@gmail.com>
2024-12-06 16:26:52 -0300

    Criado validação da criação do usuário

--- backend/middlewares/handleValidation.js
+++ backend/middlewares/handleValidation.js
@@ -1,8 +1,8 @@
 const { validationResult } = require("express-validator");
 
 const validate = (req, res, next) => {
-	const errors = validationResult(req);
-	if (errors.isEmpty) {
+	const errors = validationResult(req);    
+	if (errors.isEmpty()) {        
 		return next();
 	}
 
