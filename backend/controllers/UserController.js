c8cd070aea38c855c2ad8c2578d23c4220dafeea
Lucas <lgs.08lucas@gmail.com>
2024-12-16 19:36:33 -0300

    Completando o middleware para update do usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -78,8 +79,28 @@ const getCurrentUser = async (req, res) => {
 };
 
 // Update an user
-const update = async (rec, res) => {
-	res.send("Update");
+const update = async (req, res) => {
+	const { name, password, bio } = req.body;
+	let profileImage = null;
+	if (req.file) profileImage = req.file.filename;
+
+	const reqUser = req.user;
+	const user = await User.findById(reqUser._id).select(
+		"-password"
+	);
+
+	if (name) user.name = name;
+	if (password) {
+		// Generate password hash
+		const salt = await bcrypt.genSalt();
+		const passwordHash = await bcrypt.hash(password, salt);
+		user.password = passwordHash;
+	}
+	if (profileImage) user.profile_image = profileImage;
+	if (bio) user.bio = bio;
+
+	await user.save(); //Update
+	res.status(200).json(user);
 };
 
 module.exports = {
