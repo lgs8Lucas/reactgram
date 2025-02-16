5ce56efec36f1f49acb3337470df1c83d6b4027b
Lucas <lgs.08lucas@gmail.com>
2025-02-16 11:28:39 -0300

    Criado Navbar e Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,28 @@ import "./App.css";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+//Pages
 import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 
+//Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 function App() {
 	return (
-		<div className="App">
+		<div id="App">
 			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/register" element={<Register />} />
-				</Routes>
+				<Navbar />
+				<div className="container">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+					</Routes>
+				</div>
+				<Footer />
 			</BrowserRouter>
 		</div>
 	);
