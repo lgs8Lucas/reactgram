3f3192ddffcff4a8863cf50ce8f9aad412d2ed4b
Lucas <lgs.08lucas@gmail.com>
2025-02-16 11:49:09 -0300

    Criado estilo para ps formulários de autenticação.

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -12,7 +12,7 @@ const Register = () => {
 		e.preventDefault();
 	};
 	return (
-		<div>
+		<div id="register">
 			<h2>ReactGram</h2>
 			<p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
 			<form onSubmit={handleSubmit}>
@@ -21,10 +21,10 @@ const Register = () => {
 				<input type="password" placeholder="Senha" />
 				<input type="password" placeholder="Confirme a senha" />
 				<input type="submit" value={"Cadastrar"} />
-				<p>
-					Já tem uma conta? <Link to={"/login"}>Clique aqui.</Link>
-				</p>
 			</form>
+			<p>
+				Já tem uma conta? <Link to={"/login"}>Clique aqui.</Link>
+			</p>
 		</div>
 	);
 };
