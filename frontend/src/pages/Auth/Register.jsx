f6666e092b379d046269c8d4cf5948eeebfbad1f
Lucas <lgs.08lucas@gmail.com>
2025-02-18 15:02:03 -0300

    Implementando o redux

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -10,16 +10,49 @@ import { useState, useEffect } from "react";
 const Register = () => {
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		const user = {
+			name,
+			email,
+			password,
+			confirmPassword,
+		};
+		console.log(user);
 	};
+
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+
 	return (
 		<div id="register">
 			<h2>ReactGram</h2>
 			<p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
 			<form onSubmit={handleSubmit}>
-				<input type="text" placeholder="Nome" />
-				<input type="email" placeholder="Email" />
-				<input type="password" placeholder="Senha" />
-				<input type="password" placeholder="Confirme a senha" />
+				<input
+					type="text"
+					placeholder="Nome"
+					value={name || ""}
+					onChange={(e) => setName(e.target.value)}
+				/>
+				<input
+					type="email"
+					placeholder="Email"
+					value={email || ""}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<input
+					type="password"
+					placeholder="Senha"
+					value={password || ""}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<input
+					type="password"
+					placeholder="Confirme a senha"
+					value={confirmPassword || ""}
+					onChange={(e) => setConfirmPassword(e.target.value)}
+				/>
 				<input type="submit" value={"Cadastrar"} />
 			</form>
 			<p>
